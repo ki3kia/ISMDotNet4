a403159943aaa8fc64b84d1ae51722e5e9c1af3a
Irina <mandarinka.21.09.00@gmail.com>
2017-09-26 14:38:11 +0300

    Classes

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Matrix
+    {
+        public static void ProductMatrix (int [,] integers, int[,] matrix)
+        {
+           int [,] matr3 = new int[integers.GetLength(1), integers.GetLength(0)];
+            for (int i = 0; i < matr3.GetLength(0); i++)
+            {
+                for (int j = 0; j < matr3.GetLength(1); j++)
+                {
+                    for (int q = 0; q < integers.GetLength(0) - 1; q++)
+                    {
+                        matr3[i, j] += integers[i, q] * matrix[q, j];
+                    }
+                    Console.WriteLine($"{matr3[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Sum(int[,] integers)
+        {
+            for (int i = 0; i < integers.GetLength(0); i++)
+            {
+                for (int j = 0; j < integers.GetLength(0); j++)
+                {
+                    Console.Write($"{integers[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+        }
+         public static void SumDiagonal(int[,] integers)
+         {
+            int s = 0, z = 0;
+            for (int i = 0; i < integers.GetLength(0); i++)
+            {
+                for (int j = 0; j < integers.GetLength(1); j++)
+                {
+                    s = s + integers[i, j];
+                    Console.WriteLine(s);
+                }
+                z++;
+            }
+         }
+    }
+}
